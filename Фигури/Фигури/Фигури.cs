061c6f2f757cb68918c18ce3a37852aad4a43442
Nataliq-Vulkova <nataliavalkova2009@gmail.com>
2024-12-09 13:39:50 +0200

    Add files via upload

---
+++ Фигури/Фигури/Фигури.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фигури
+{
+    internal class Фигури
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            var n=int.Parse(Console.ReadLine());
+            for (var i = 1; i < n; i++)
+            {
+                for (var a = 1; a < n - 2; a++)
+                    Console.WriteLine("+ - +");
+                Console.WriteLine("| - |");
+                for (var a = 1; a < i ; a++)
+                    Console.WriteLine("+ - +");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Числа/Числа/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" *    *        *       * * * * * *     *        *  ");
+            Console.WriteLine(" *    *       *  *          *          *      * *  ");
+            Console.WriteLine(" * ** *      * ** *         *          *    *   *  ");
+            Console.WriteLine(" *    *     *      *        *          *  *     *  ");
+            Console.WriteLine(" *    *    *        *       *          *        *  ");
+
+
+            Console.Write("Как се казваш?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Здравей, {name}!");
+            Console.Write("Как е името на номер 17");
+            string name1 = Console.ReadLine();
+            Console.WriteLine($"Може би, {name1} ще се опита да завърши");
+
+        }
+    }
+}
